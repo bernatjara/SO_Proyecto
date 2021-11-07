4ecc9d9df468cd8e30e9889c755ea860eb764a58
Bernat Jara Ortínez <bernat.jara@estudiantat.upc.edu>
2021-11-07 17:46:32 +0100

    Proyecto SO con mutex en C

---
+++ Versio1/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Versio1
+{
+    public partial class Form1 : Form
+    {
+        Socket server;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int iniciar = 0;
+        int sal = 0;
+        private void enviar_Click(object sender, EventArgs e)
+        {
+            if (iniciar == 1)
+            {
+                if (ganador.Checked)
+                {
+                    // Quiere la longitud del nombre
+                    string mensaje = "1/";
+                    // Enviamos al servidor el nombre
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                    server.Send(msg);
+                    //Recibimos la respuesta del servidor
+                    byte[] msg2 = new byte[80];
+                    server.Receive(msg2);
+                    mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                    MessageBox.Show(mensaje);
+                }
+                if (Puntos.Checked)
+                {
+                    if ((string.IsNullOrEmpty(Nombre.Text)) || (string.IsNullOrEmpty(Data.Text)))
+                    {
+                        MessageBox.Show("Introduzca su nombre y data porfavor.");
+                    }
+                    else
+                    {
+                        // Quiere la longitud del nombre
+                        string mensaje = "2/" + Nombre.Text+ "/" + Data.Text;
+                        // Enviamos al servidor el nombre
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                        server.Send(msg);
+                        //Recibimos la respuesta del servidor
+                        byte[] msg2 = new byte[80];
+                        server.Receive(msg2);
+                        mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                        MessageBox.Show(mensaje);
+                    }
+                }
+                if (nganador.Checked)
+                {
+                    if (string.IsNullOrEmpty(Nombre.Text))
+                    {
+                        MessageBox.Show("Introduzca su nombre porfavor.");
+                    }
+                    else
+                    {
+                        // Quiere la longitud del nombre
+                        string mensaje = "3/" + Nombre.Text;
+                        // Enviamos al servidor el nombre
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                        server.Send(msg);
+                        //Recibimos la respuesta del servidor
+                        byte[] msg2 = new byte[80];
+                        server.Receive(msg2);
+                        mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                        MessageBox.Show(mensaje);
+                    }
+                }
+            }
+            else
+                MessageBox.Show("No has iniciado sesión aún.");
+        }
+
+        private void salir_Click(object sender, EventArgs e)
+        {
+            if (sal == 1)
+            {
+                //Enviamos mensaje de desconexión
+                string mensaje = "0/";
+                byte[] msg2 = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg2);
+
+                // Nos desconectamos
+                this.BackColor = Color.Gray;
+                server.Shutdown(SocketShutdown.Both);
+                iniciar = 0;
+                sal = 0;
+                server.Close();
+                Close();
+            }
+            else
+            {
+                iniciar = 0;
+                sal = 0;
+                Close();
+            }
+        }
+
+        private void Registarse_Click(object sender, EventArgs e)
+        {
+            if (this.BackColor != Color.Green)
+            {
+                //IPEndPoint con el ip y el puerto del servidor al que queremos conectarnos
+                IPAddress direc = IPAddress.Parse("192.168.56.102");
+                IPEndPoint ipep = new IPEndPoint(direc, 9265);
+
+                //Creamos el socket 
+                server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    //Intentamos conectar el socket
+                    server.Connect(ipep);
+                }
+
+                catch (SocketException)
+                {
+                    //Si hay excepcion imprimimos error y salimos del programa con return 
+                    MessageBox.Show("No he podido conectar con el servidor");
+                    return;
+                }
+                if ((string.IsNullOrEmpty(usuario.Text)) || (string.IsNullOrEmpty(contraseña.Text)))
+                {
+                    MessageBox.Show("Introduzca su usuario y contraseña porfavor.");
+                }
+                else
+                {
+                    // Quiere la longitud del nombre
+                    string mensaje = "4/" + usuario.Text + "/" + contraseña.Text;
+                    // Enviamos al servidor el nombre
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                    server.Send(msg);
+
+                    //Recibimos la respuesta del servidor
+                    byte[] msg2 = new byte[80];
+                    server.Receive(msg2);
+                    mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                    MessageBox.Show(mensaje);
+                    //Enviamos mensajje de desconexión
+                    string mensa = "0/";
+                    byte[] msg3 = System.Text.Encoding.ASCII.GetBytes(mensa);
+                    server.Send(msg3);
+                    server.Shutdown(SocketShutdown.Both);
+                    server.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("No te puedes registrar con una sesión iniciada.");
+            }
+        }
+
+        private void Logearse_Click(object sender, EventArgs e)
+        {
+            if (this.BackColor != Color.Green)
+            {
+                //IPEndPoint con el ip y el puerto del servidor al que queremos conectarnos
+                IPAddress direc = IPAddress.Parse("192.168.56.102");
+                IPEndPoint ipep = new IPEndPoint(direc, 9265);
+
+                //Creamos el socket 
+                server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    //Intentamos conectar el socket
+                    server.Connect(ipep);
+                    if ((string.IsNullOrEmpty(usuario.Text)) || (string.IsNullOrEmpty(contraseña.Text)))
+                    {
+                        MessageBox.Show("Introduzca su usuario y contraseña porfavor.");
+                    }
+                    else
+                    {
+                        // Quiere la longitud del nombre
+                        string mensaje = "5/" + usuario.Text + "/" + contraseña.Text;
+                        // Enviamos al servidor el nombre
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                        server.Send(msg);
+
+                        //Recibimos la respuesta del servidor
+                        byte[] msg2 = new byte[80];
+                        server.Receive(msg2);
+                        mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                        if (mensaje == "Si")
+                        {
+                            this.BackColor = Color.Green;
+                            MessageBox.Show("Bienvindo usuario.");
+                            iniciar = 1;
+                            sal = 1;
+                        }
+                        else
+                        {
+                            //Enviamos mensaje de desconexión
+                            string mensaj = "0/";
+                            byte[] msg3 = System.Text.Encoding.ASCII.GetBytes(mensaj);
+                            server.Send(msg3);
+
+                            // Nos desconectamos
+                            server.Shutdown(SocketShutdown.Both);
+                            server.Close();
+                            MessageBox.Show("El usuario o la contraseña son incorrectas.");
+                        }
+                    }
+                }
+                catch (SocketException)
+                {
+                    //Si hay excepcion imprimimos error y salimos del programa con return 
+                    MessageBox.Show("No he podido conectar con el servidor");
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("No puedes iniciar sesión sin desconectarte primero de la actual.");
+            }
+        }
+
+        private void desco_Click(object sender, EventArgs e)
+        {
+            if (this.BackColor == Color.Green)
+            {
+                //Enviamos mensaje de desconexión
+                string mensaje = "0/";
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                // Nos desconectamos
+                this.BackColor = Color.Gray;
+                iniciar = 0;
+                sal = 0;
+                server.Shutdown(SocketShutdown.Both);
+                server.Close();
+            }
+            else
+            {
+                MessageBox.Show("Primero te debes conectar para que te puedas desconectar.");
+            }
+        }
+    }
+}
---
+++ bbdd.sql
@@ -0,0 +1,58 @@
+DROP DATABASE IF EXISTS BaseDatos;
+CREATE DATABASE BaseDatos;
+
+USE BaseDatos;
+
+CREATE TABLE Jugador(
+	ID INT NOT NULL AUTO_INCREMENT,
+	Nombre VARCHAR(60),
+	Password VARCHAR(60),
+	Victoria INT NOT NULL,
+	PRIMARY KEY (ID)
+)ENGINE=InnoDB;
+
+CREATE TABLE Partida(
+	ID INT NOT NULL AUTO_INCREMENT,
+	Fecha VARCHAR(10),	/*YYYY-MM-DD*/
+	Hora VARCHAR(10),	/*HH: MM: SS*/
+	Duracion INT,		/*Duración en minutos*/
+	Ganador VARCHAR(60),
+	PRIMARY KEY (ID)
+)ENGINE=InnoDB;
+
+CREATE TABLE Partidas(
+	ID_J INT,
+	ID_P INT,
+	Puntuacion INT,
+	FOREIGN KEY (ID_J) REFERENCES Jugador(ID),
+	FOREIGN KEY (ID_P) REFERENCES Partida(ID)
+)ENGINE=InnoDB;
+
+INSERT INTO Jugador(Nombre, Password, Victoria) VALUES ('Bernat','12',1); 
+INSERT INTO Jugador(Nombre, Password, Victoria) VALUES ('Jordi','34',1);
+INSERT INTO Jugador(Nombre, Password, Victoria) VALUES ('Nil','56',1);
+INSERT INTO Jugador(Nombre, Password, Victoria) VALUES ('Miguel','78',0);
+INSERT INTO Jugador(Nombre, Password, Victoria) VALUES ('Eda','51',2);
+INSERT INTO Jugador(Nombre, Password, Victoria) VALUES ('Pablo','88',0);
+INSERT INTO Jugador(Nombre, Password, Victoria) VALUES ('Aitana','98',1);
+INSERT INTO Jugador(Nombre, Password, Victoria) VALUES ('Aida','345',0);
+
+INSERT INTO Partida(Fecha, Hora, Duracion, Ganador) VALUES ('2008-07-01','00: 01: 59',120,'Eda');
+INSERT INTO Partida(Fecha, Hora, Duracion, Ganador) VALUES ('2010-08-22','10: 59: 59',60,'Aitana');
+INSERT INTO Partida(Fecha, Hora, Duracion, Ganador) VALUES ('2005-05-05','22: 30: 15',14,'Bernat');
+INSERT INTO Partida(Fecha, Hora, Duracion, Ganador) VALUES ('2020-07-01','00: 17: 00',40,'Nil');
+INSERT INTO Partida(Fecha, Hora, Duracion, Ganador) VALUES ('2008-07-15','08: 00: 00',120,'Eda');
+INSERT INTO Partida(Fecha, Hora, Duracion, Ganador) VALUES ('2009-01-01','20: 00: 00',120,'Jordi');
+
+INSERT INTO Partidas(ID_J, ID_P, Puntuacion) VALUES (1,1,11);
+INSERT INTO Partidas(ID_J, ID_P, Puntuacion) VALUES (5,1,25);
+INSERT INTO Partidas(ID_J, ID_P, Puntuacion) VALUES (7,2,60);
+INSERT INTO Partidas(ID_J, ID_P, Puntuacion) VALUES (2,2,59);
+INSERT INTO Partidas(ID_J, ID_P, Puntuacion) VALUES (1,3,14);
+INSERT INTO Partidas(ID_J, ID_P, Puntuacion) VALUES (8,3,12);
+INSERT INTO Partidas(ID_J, ID_P, Puntuacion) VALUES (3,4,40);
+INSERT INTO Partidas(ID_J, ID_P, Puntuacion) VALUES (7,4,35);
+INSERT INTO Partidas(ID_J, ID_P, Puntuacion) VALUES (7,5,11);
+INSERT INTO Partidas(ID_J, ID_P, Puntuacion) VALUES (5,5,12);
+INSERT INTO Partidas(ID_J, ID_P, Puntuacion) VALUES (2,6,11);
+INSERT INTO Partidas(ID_J, ID_P, Puntuacion) VALUES (6,6,5);
